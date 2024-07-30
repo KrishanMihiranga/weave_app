1f58009d645de9de6cc7c6c9ad30a8dd1011e493
KrishanMihiranga <xp3cto95@gmail.com>
2024-07-30 20:02:53 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,33 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ClerkProvider>
+          <Topbar />
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+    publicRoutes: ['/', '/api/webhook/clerk', '/api/uploadthing'],
+    ignoredRoutes: ['/api/webhook/clerk', '/api/uploadthing']
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
